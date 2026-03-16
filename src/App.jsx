c98faf3c5e29c6a74b79d0af2d26a90df9bc4bf5
DESKTOP-O5IS8PT <vasjovan7@gmail.com>
2026-03-16 02:21:26 +0100

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -35,9 +35,7 @@ function App() {
         <Route path="/admin/tournament/:id" element={<TournamentTeams />} />
 
 <Route path="/team/:matchId/:teamId" element={<TeamPanel />} />
-<Route path="/team" element={<div>Morate otvoriti /team/matchId/teamId</div>} />
-<Route path="/team/:matchId/:teamId" element={<TeamPanel />} />
-      </Routes>
+   </Routes>
     </BrowserRouter>
   );
 }
